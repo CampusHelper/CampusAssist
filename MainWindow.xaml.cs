9e527276c30948e3162a4bc5f9c4b49515b7b099
KaitoHH <hh.kaito@gmail.com>
2015-12-14 14:17:32 +0800

    添加主界面窗口

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,31 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CampusAssist
 {
     /// <summary>
-    /// LoginWindow.xaml 的交互逻辑
+    /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class LoginWindow : Window
+    public partial class MainWindow : Window
     {
-        public LoginWindow()
+        public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void onExit(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void onLogin(object sender, RoutedEventArgs e)
-        {
-            MainWindow wd = new MainWindow();
-            wd.Show();
-            Close();
         }
     }
 }
