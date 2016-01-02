afe4a4663f59dc7c2e9325c051727cfdf55be8ba
Giraffa0215 <1175522712@qq.com>
2016-01-02 22:15:57 +0800

    接收文件

--- Email.Designer.cs
+++ Email.Designer.cs
@@ -28,33 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Connect = new System.Windows.Forms.Button();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Disconnect = new System.Windows.Forms.Button();
+            this.Retrieve = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // button1
+            // Connect
             // 
-            this.button1.Location = new System.Drawing.Point(235, 107);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Connect.Location = new System.Drawing.Point(315, 69);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 0;
+            this.Connect.Text = "连接";
+            this.Connect.UseVisualStyleBackColor = true;
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(113, 69);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(100, 21);
+            this.Username.TabIndex = 1;
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(113, 116);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(100, 21);
+            this.Password.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "用户名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "密码：";
+            // 
+            // Disconnect
+            // 
+            this.Disconnect.Location = new System.Drawing.Point(315, 116);
+            this.Disconnect.Name = "Disconnect";
+            this.Disconnect.Size = new System.Drawing.Size(75, 23);
+            this.Disconnect.TabIndex = 5;
+            this.Disconnect.Text = "断开连接";
+            this.Disconnect.UseVisualStyleBackColor = true;
+            // 
+            // Retrieve
+            // 
+            this.Retrieve.Location = new System.Drawing.Point(463, 161);
+            this.Retrieve.Name = "Retrieve";
+            this.Retrieve.Size = new System.Drawing.Size(75, 23);
+            this.Retrieve.TabIndex = 6;
+            this.Retrieve.Text = "收取文件";
+            this.Retrieve.UseVisualStyleBackColor = true;
+            // 
+            // Status
+            // 
+            this.Status.FormattingEnabled = true;
+            this.Status.ItemHeight = 12;
+            this.Status.Location = new System.Drawing.Point(92, 219);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(195, 88);
+            this.Status.TabIndex = 7;
             // 
             // Email
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(675, 522);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(675, 369);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Retrieve);
+            this.Controls.Add(this.Disconnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.Connect);
             this.Name = "Email";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Email_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Disconnect;
+        private System.Windows.Forms.Button Retrieve;
+        private System.Windows.Forms.ListBox Status;
     }
 }
--- Email.cs
+++ Email.cs
@@ -7,14 +7,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Cryptography;
+using System.IO;
+
 
 namespace CampusAssist
 {
     public partial class Email : Form
-    {
+    {    
+        public TcpClient Server;
+        public NetworkStream NetStrm;
+        public StreamReader RdStrm;
+        public string Data;
+        public byte[] szData;
+        public string CRLF = "\r\n";
+
         public Email()
         {
             InitializeComponent();
         }
+
+        private void Username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Connect_Click(object sender, System.EventArgs e)
+        {
+            //将光标置为等待状态 
+            Cursor cr = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+
+            //用110端口新建POP3服务器连接 
+            Server = new TcpClient("smail.ecnu.edu.cn", 110);
+            Status.Items.Clear();
+
+            try
+            {
+                //初始化 
+                NetStrm = Server.GetStream();
+                RdStrm = new StreamReader(Server.GetStream());
+                Status.Items.Add(RdStrm.ReadLine());
+
+                //登录服务器过程 
+                Data = "USER " + Username.Text + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                Status.Items.Add(RdStrm.ReadLine());
+
+                Data = "PASS " + Password.Text + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                Status.Items.Add(RdStrm.ReadLine());
+
+                //向服务器发送STAT命令，从而取得邮箱的相关信息：邮件数量和大小 
+                Data = "STAT" + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                Status.Items.Add(RdStrm.ReadLine());
+
+                //改变按钮的状态 
+                Connect.Enabled = false;
+                Disconnect.Enabled = true;
+                Retrieve.Enabled = true;
+
+                //将光标置回原来的状态 
+                Cursor.Current = cr;
+
+            }
+            catch (InvalidOperationException err)
+            {
+                Status.Items.Add("Error: " + err.ToString());
+            }
+        }
+
+        private void Email_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
+
+ 
 }
