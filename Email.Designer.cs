f1c58eb51c7fa6ffd245dfc546317b66d63d2996
payonxp <522983486@qq.com>
2016-01-04 01:50:33 +0800

    pop3

--- Email.Designer.cs
+++ Email.Designer.cs
@@ -28,41 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Connect = new System.Windows.Forms.Button();
             this.Username = new System.Windows.Forms.TextBox();
             this.Password = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.Disconnect = new System.Windows.Forms.Button();
-            this.Retrieve = new System.Windows.Forms.Button();
-            this.Status = new System.Windows.Forms.ListBox();
-            this.Message = new System.Windows.Forms.RichTextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.MailNum = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // Connect
-            // 
-            this.Connect.Location = new System.Drawing.Point(315, 69);
-            this.Connect.Name = "Connect";
-            this.Connect.Size = new System.Drawing.Size(75, 23);
-            this.Connect.TabIndex = 0;
-            this.Connect.Text = "连接";
-            this.Connect.UseVisualStyleBackColor = true;
-            this.Connect.Click += new System.EventHandler(this.Connect_Click);
             // 
             // Username
             // 
-            this.Username.Location = new System.Drawing.Point(113, 69);
+            this.Username.Location = new System.Drawing.Point(113, 26);
             this.Username.Name = "Username";
             this.Username.Size = new System.Drawing.Size(100, 21);
             this.Username.TabIndex = 1;
             // 
             // Password
             // 
-            this.Password.Location = new System.Drawing.Point(113, 116);
+            this.Password.Location = new System.Drawing.Point(113, 66);
             this.Password.Name = "Password";
             this.Password.Size = new System.Drawing.Size(100, 21);
             this.Password.TabIndex = 2;
@@ -70,7 +52,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 69);
+            this.label1.Location = new System.Drawing.Point(45, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 3;
@@ -79,101 +61,32 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 116);
+            this.label2.Location = new System.Drawing.Point(45, 69);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 12);
             this.label2.TabIndex = 4;
             this.label2.Text = "密码：";
             // 
-            // Disconnect
+            // button1
             // 
-            this.Disconnect.Location = new System.Drawing.Point(315, 116);
-            this.Disconnect.Name = "Disconnect";
-            this.Disconnect.Size = new System.Drawing.Size(75, 23);
-            this.Disconnect.TabIndex = 5;
-            this.Disconnect.Text = "断开连接";
-            this.Disconnect.UseVisualStyleBackColor = true;
-            this.Disconnect.Click += new System.EventHandler(this.Disconnect_Click);
-            // 
-            // Retrieve
-            // 
-            this.Retrieve.Location = new System.Drawing.Point(471, 191);
-            this.Retrieve.Name = "Retrieve";
-            this.Retrieve.Size = new System.Drawing.Size(75, 23);
-            this.Retrieve.TabIndex = 6;
-            this.Retrieve.Text = "收取文件";
-            this.Retrieve.UseVisualStyleBackColor = true;
-            this.Retrieve.Click += new System.EventHandler(this.Retrieve_Click);
-            // 
-            // Status
-            // 
-            this.Status.FormattingEnabled = true;
-            this.Status.ItemHeight = 12;
-            this.Status.Location = new System.Drawing.Point(64, 343);
-            this.Status.Name = "Status";
-            this.Status.Size = new System.Drawing.Size(317, 76);
-            this.Status.TabIndex = 7;
-            // 
-            // Message
-            // 
-            this.Message.Location = new System.Drawing.Point(64, 172);
-            this.Message.Name = "Message";
-            this.Message.Size = new System.Drawing.Size(382, 132);
-            this.Message.TabIndex = 8;
-            this.Message.Text = "";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(45, 157);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 12);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "信息：";
-            // 
-            // MailNum
-            // 
-            this.MailNum.Location = new System.Drawing.Point(471, 288);
-            this.MailNum.Name = "MailNum";
-            this.MailNum.Size = new System.Drawing.Size(75, 21);
-            this.MailNum.TabIndex = 10;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(45, 328);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 12);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "状态：";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(469, 273);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 12);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "邮件编号：";
+            this.button1.Location = new System.Drawing.Point(251, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Email
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(675, 445);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.MailNum);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.Message);
-            this.Controls.Add(this.Status);
-            this.Controls.Add(this.Retrieve);
-            this.Controls.Add(this.Disconnect);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Password);
             this.Controls.Add(this.Username);
-            this.Controls.Add(this.Connect);
             this.Name = "Email";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -184,19 +97,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button Connect;
         private System.Windows.Forms.TextBox Username;
         private System.Windows.Forms.TextBox Password;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button Disconnect;
-        private System.Windows.Forms.Button Retrieve;
-        private System.Windows.Forms.ListBox Status;
-        private System.Windows.Forms.RichTextBox Message;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox MailNum;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Email.cs
+++ Email.cs
@@ -15,150 +15,151 @@ using System.IO;
 
 namespace CampusAssist
 {
-    public partial class Email : Form
-    {    
-        public TcpClient Server;
-        public NetworkStream NetStrm;
-        public StreamReader RdStrm;
-        public string Data;
-        public byte[] szData;
-        public string CRLF = "\r\n";
+    struct mail                             //邮件结构体
+    {
+        public string fromAddress;          //发信地址
+        public string fromName;             //发信人
+        public string subject;              //主题
+        public string body;                 //正文
+        public string time;                 //日期 时间
+        public bool read;                   //已读/未读
+    };
 
+    public partial class Email : Form
+    {
+        private string userName = "";           //用户名
+        private string pwd = "";                //密码
+        mail[] mailList = null;                 //存储邮件
+        int all_num=0;                            //目前所有邮件数量
+        int new_num=0;                            //新的邮件数量
         public Email()
         {
             InitializeComponent();
         }
 
-        private void Username_TextChanged(object sender, EventArgs e)
-        {}
-
-        private void Connect_Click(object sender, System.EventArgs e)
-        {
-            //将光标置为等待状态 
-            Cursor cr = Cursor.Current;
-            Cursor.Current = Cursors.WaitCursor;
-
-            //用110端口新建POP3服务器连接 
-            Server = new TcpClient("pop.163.com", 110);
-            Status.Items.Clear();
-
-            try
-            {
-                //初始化 
-                NetStrm = Server.GetStream();
-                RdStrm = new StreamReader(Server.GetStream());
-                Status.Items.Add(RdStrm.ReadLine());
-
-                //登录服务器过程 
-                Data = "USER " + Username.Text + CRLF;
-                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
-                NetStrm.Write(szData, 0, szData.Length);
-                Status.Items.Add(RdStrm.ReadLine());
-
-                Data = "PASS " + Password.Text + CRLF;
-                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
-                NetStrm.Write(szData, 0, szData.Length);
-                Status.Items.Add(RdStrm.ReadLine());
-
-                //向服务器发送STAT命令，从而取得邮箱的相关信息：邮件数量和大小 
-                Data = "STAT" + CRLF;
-                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
-                NetStrm.Write(szData, 0, szData.Length);
-                Status.Items.Add(RdStrm.ReadLine());
-
-                //改变按钮的状态 
-                Connect.Enabled = false;
-                Disconnect.Enabled = true;
-                Retrieve.Enabled = true;
-
-                //将光标置回原来的状态 
-                Cursor.Current = cr;
-
-            }
-            catch (InvalidOperationException err)
-            {
-                Status.Items.Add("Error: " + err.ToString());
-            }
-        }
-
         private void Email_Load(object sender, EventArgs e)
-        {}
-
-        private void Disconnect_Click(object sender, EventArgs e)
         {
-            //将光标置为等待状态 
-            Cursor cr = Cursor.Current;
-            Cursor.Current = Cursors.WaitCursor;
-
-            //向服务器发送QUIT命令从而结束和POP3服务器的会话 
-            Data = "QUIT" + CRLF;
-            szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
-            NetStrm.Write(szData, 0, szData.Length);
-            Status.Items.Add(RdStrm.ReadLine());
-
-            //断开连接 
-            NetStrm.Close();
-            RdStrm.Close();
-
-            //改变按钮的状态 
-            Connect.Enabled = true;
-            Disconnect.Enabled = false;
-            Retrieve.Enabled = false;
-
-            //将光标置回原来的状态 
-            Cursor.Current = cr; 
         }
 
-        private void Retrieve_Click(object sender, EventArgs e)
+        public void getEmail(int emailNum)         //获取email_num封邮件，将邮件总数保存到all_num
         {
-            //将光标置为等待状态 
-            Cursor cr = Cursor.Current;
-            Cursor.Current = Cursors.WaitCursor;
-            string szTemp;
-            Message.Clear();
-
-            try
+            Chilkat.MailMan mailman = new Chilkat.MailMan();
+            bool success = mailman.UnlockComponent("30-day trial");
+            if (success != true)
             {
-                //根据邮件编号从服务器获得相应邮件 
-                Data = "RETR " + MailNum.Text + CRLF;
-                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
-                NetStrm.Write(szData, 0, szData.Length);
-                szTemp = RdStrm.ReadLine();
-
-                if (szTemp[0] != '-')
-                {
-                    //不断地读取邮件内容，只到结束标志：英文句号 
-                    while (szTemp != ".")
-                    {
-                        Message.Text += szTemp;
-                        szTemp = RdStrm.ReadLine();
-                    }
-
-                    //若BackupChBox未选中，则收取邮件后，删除保留在服务器上的邮件 
-                   /* if (BackupChBox.Checked == false)
-                    {
-                        Data = "DELE" + MailNum.Text + CRLF;
-                        szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
-                        NetStrm.Write(szData, 0, szData.Length);
-                        Status.Items.Add(RdStrm.ReadLine());
-                    }*/
-                }
-                else
-                {
-                    Status.Items.Add(szTemp);
-                }
-
-                //将光标置回原来的状态 
-                Cursor.Current = cr;
+                Console.WriteLine("Component unlock failed");
+                return;
             }
-            catch (InvalidOperationException err)
+            mailman.MailHost = "smail.ecnu.edu.cn";
+            mailman.PopUsername = userName;
+            mailman.PopPassword = pwd;
+
+            Chilkat.EmailBundle bundle = null;
+            bundle = mailman.CopyMail();
+            if (bundle == null)
             {
-                Status.Items.Add("Error: " + err.ToString());
+                Console.WriteLine(mailman.LastErrorText);
+                return;
+            }
+            mailList = new mail[emailNum];
+            int count = 0;
+            Chilkat.Email email = null;
+            for (int i = bundle.MessageCount - 1; i >= bundle.MessageCount - 1 - emailNum; i--)
+            {
+                email = bundle.GetEmail(i);
+                DateTime time = email.LocalDate;
+                string timeStr = time.ToString();
+                mailList[count].fromAddress = email.FromAddress;
+                mailList[count].fromName = email.FromName;
+                mailList[count].subject = email.Subject;
+                mailList[count].time = timeStr;
+                mailList[count].body = email.Body;
+                count++;
+            }
+            mailman.Pop3EndSession();
+
+            all_num = bundle.MessageCount;
+
+            return;
+        }
+
+        public void getNew(int emailNum)            //获取emailNum封邮件，如果邮件总数增加，将增加数保存到new_num
+        {
+            int oldNum = all_num;
+            getEmail(emailNum);
+            if(all_num >= oldNum)
+            {
+                new_num = all_num - oldNum;
             }
         }
 
+        public string getFromAddress(int index)     //获取第index封邮件的发信地址
+        {
+            if (index >= mailList.Length)
+            {
+                MessageBox.Show("Out of range!");
+                return null;
+            }
+            return mailList[index].fromAddress;
+        }
 
+        public string getFromName(int index)     //获取第index封邮件的发信人
+        {
+            if (index >= mailList.Length)
+            {
+                MessageBox.Show("Out of range!");
+                return null;
+            }
+            return mailList[index].fromName;
+        }
 
+        public string getSubject(int index)     //获取第index封邮件的主题
+        {
+            if (index >= mailList.Length)
+            {
+                MessageBox.Show("Out of range!");
+                return null;
+            }
+            return mailList[index].subject;
+        }
+
+        public string getBody(int index)     //获取第index封邮件的正文
+        {
+            if (index >= mailList.Length)
+            {
+                MessageBox.Show("Out of range!");
+                return null;
+            }
+            return mailList[index].body;
+        }
+
+        public string getTime(int index)     //获取第index封邮件的发信 日期 时间
+        {
+            if (index >= mailList.Length)
+            {
+                MessageBox.Show("Out of range!");
+                return null;
+            }
+            return mailList[index].time;
+        }
+
+        public bool getRead(int index)     //获取第index封邮件已读/未读
+        {
+            if (index >= mailList.Length)
+            {
+                MessageBox.Show("Out of range!");
+                return false;
+            }
+            return mailList[index].read;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userName = "10142510186";
+            pwd = "payon951224";
+            getEmail(10);
+            MessageBox.Show(getBody(2));
+        }
     }
 
  
