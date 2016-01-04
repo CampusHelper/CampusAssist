af59ddef626c9183255b30acd05ea693029d161e
KaitoHH <hh.kaito@gmail.com>
2016-01-04 14:16:00 +0800

    课程表实现

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,14 @@ namespace CampusAssist
         private WebProcess web;
         bool onInit;
         string[] weekdays = { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+        byte[,] color = { { 96, 192, 230 },
+                        { 147, 209, 98 },
+                        { 136, 172, 234 },
+                        { 117, 192, 172 },
+                        { 226, 151, 181 },
+                        { 132, 216, 164 },
+                        { 130, 211, 212 },
+                        { 245, 172, 139} };
         int[,] semesterId = new int[,] { { 449, 481 }, { 385, 417 }, { 321, 353 }, { 89, 288 } };
         int[] examType = new int[] { 1, 3, 4, 2, 100 };
         System.Windows.Forms.NotifyIcon notify;
@@ -36,10 +44,10 @@ namespace CampusAssist
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-/* 一以下最好开新的线程处理否则会阻塞 */
+            /* 一以下最好开新的线程处理否则会阻塞 */
             Thread oThread = new Thread(init);
             oThread.Start();
-            /* 以上 */            
+            /* 以上 */
             dateLbl.Content = DateTime.Now.ToLongDateString().ToString() + " " + weekdays[Convert.ToInt32(DateTime.Now.DayOfWeek)];
             notify = new System.Windows.Forms.NotifyIcon();
             notify.Icon = new System.Drawing.Icon("icon.ico");
@@ -134,15 +142,25 @@ namespace CampusAssist
         {
             if (!onInit)
             {
+                examGrid.Children.RemoveRange(6, examGrid.Children.Count - 6);
                 int semeId = 0;
                 semeId = semesterId[yearCombo.SelectedIndex, semesterCombo.SelectedIndex];
-                MessageBox.Show(semeId.ToString());
                 string url = String.Format("http://applicationnewjw.ecnu.edu.cn/eams/teach/grade/course/person!search.action?semesterId={0}", semeId);
                 string page = web.getDocument(url, Encoding.UTF8);
                 string[] exam = HtmlParse.getScores(page);
                 for (int i = 0; i < exam.Length; i++)
                 {
-                    MessageBox.Show(exam[i]);
+                    string[] cur = exam[i].Split(' ');
+                    examGrid.RowDefinitions.Add(new RowDefinition());
+                    for (int j = 0; j < cur.Length; j++)
+                    {
+                        Label lbl = new Label();
+                        lbl.FontSize = 16;
+                        lbl.Content = cur[j];
+                        examGrid.Children.Add(lbl);
+                        Grid.SetRow(lbl, i + 1);
+                        Grid.SetColumn(lbl, j);
+                    }
                 }
             }
         }
@@ -182,14 +200,27 @@ namespace CampusAssist
         {
             if (!onInit)
             {
-                examTimeGrid.Children.RemoveRange(6, examTimeGrid.Children.Count - 6);
+                classGrid.Children.RemoveRange(17, classGrid.Children.Count - 17);
+
                 int semeId = semesterId[yearClassCombo.SelectedIndex, semesterClassCombo.SelectedIndex];
-                string url = String.Format("http://applicationnewjw.ecnu.edu.cn/eams/courseTableForStd!courseTable.action?ignoreHead=1&setting.kind=std&semester.id={0}&ids={1}", semeId, 260366);
+                string url = String.Format("http://applicationnewjw.ecnu.edu.cn/eams/courseTableForStd!courseTable.action?ignoreHead=1&setting.kind=std&semester.id={0}&ids={1}&startWeek={2}", semeId, 260366, weekCombo.SelectedIndex + 1);
                 string page = web.getDocument(url, Encoding.UTF8);
                 string[] schedule = HtmlParse.getSchedule(page);
-                for (int i = 0; i <schedule.Length; i++)
+                for (int i = 0; i < schedule.Length; i++)
                 {
-                    MessageBox.Show(schedule[i]);
+                    string[] cur = schedule[i].Split(' ');
+                    TextBlock lbl = new TextBlock();
+                    lbl.Text = cur[1];
+                    lbl.FontSize = 20;
+                    lbl.Background = new SolidColorBrush(Color.FromRgb(color[i%8, 0], color[i%8, 1], color[i%8, 2]));
+                    lbl.TextWrapping = TextWrapping.Wrap;
+                    classGrid.Children.Add(lbl);
+                    int y = int.Parse(cur[3]);
+                    int x = int.Parse(cur[4]);
+                    int d = int.Parse(cur[5]);
+                    Grid.SetRow(lbl, x);
+                    Grid.SetColumn(lbl, y);
+                    Grid.SetRowSpan(lbl, d);
                 }
             }
         }
@@ -198,21 +229,24 @@ namespace CampusAssist
         {
             string mainPage = web.doRedict();           // 首次进入需要重定向一次
             string[] info = HtmlParse.getInfo(mainPage);
-            Dispatcher.Invoke((Action)delegate {
+            Dispatcher.Invoke((Action)delegate
+            {
                 nameLbl.Content = info[0];
                 ipLbl.Content = info[1];
             });
             string html = web.getDocument("http://applicationidc.ecnu.edu.cn/ecnuidc/sso/ssoemailchh.jsp", Encoding.Default);
             info = HtmlParse.getEmail(html);
-            Dispatcher.Invoke((Action)delegate {
+            Dispatcher.Invoke((Action)delegate
+            {
                 mailCntLbl.Content = info[0];
                 unreadCntLbl.Content = info[1];
             });
             string balance = getBalance();
             if (double.Parse(balance) < 10)
             {
-                Dispatcher.Invoke((Action)delegate {
-                    balanceMainLbl.Content = "你的校园卡余额不足，请及时充值！";
+                Dispatcher.Invoke((Action)delegate
+                {
+                    balanceMainLbl.Content = "你的校园卡余额不足10元，请及时充值！";
                 });
             }
         }
@@ -236,8 +270,12 @@ namespace CampusAssist
 
             page = web.getDocument("http://www.ecard.ecnu.edu.cn/Ecard/cqmoney.aspx", Encoding.Default);
             string[] consume = HtmlParse.getConsume(page);
-            Dispatcher.Invoke((Action)delegate {
+            Dispatcher.Invoke((Action)delegate
+            {
                 recordProcessLbl.Content = "[正在获取中...]";
+            });
+            Dispatcher.Invoke((Action)delegate
+            {
                 costGrid.Children.RemoveRange(6, costGrid.Children.Count - 6);
                 for (int i = 0; i < consume.Length; i++)
                 {
@@ -255,7 +293,7 @@ namespace CampusAssist
                 }
                 recordProcessLbl.Content = "";
             });
-            
+
         }
     }
 }
